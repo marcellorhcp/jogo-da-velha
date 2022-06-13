38b52c1eaccfd2696f1f2724788dd3c180ad784e
Marcelo Azevedo <marcellorhcp@gmail.com>
2022-06-13 17:55:16 -0300

    refatorando função iniciaJogo

--- iniciaJogo.go
+++ iniciaJogo.go
@@ -3,33 +3,28 @@ package main
 func IniciaJogo() (bool, int) {
 	// var rodadas = 9
 
-	ProximaJogada(jogador1.player, jogador1.caractere)
-	ProximaJogada(jogador2.player, jogador2.caractere)
-	ProximaJogada(jogador1.player, jogador1.caractere)
-	ProximaJogada(jogador2.player, jogador2.caractere)
+	ExecutaJogada(jogador1.player, jogador1.caractere)
+	ExecutaJogada(jogador2.player, jogador2.caractere)
+	ExecutaJogada(jogador1.player, jogador1.caractere)
+	ExecutaJogada(jogador2.player, jogador2.caractere)
 
-	ProximaJogada(jogador1.player, jogador1.caractere)
-	if VerificaVencedor(matriz) {
+	if ExecutaJogada(jogador1.player, jogador1.caractere) {
 		return true, jogador1.player
 	}
 
-	ProximaJogada(jogador2.player, jogador2.caractere)
-	if VerificaVencedor(matriz) {
+	if ExecutaJogada(jogador2.player, jogador2.caractere) {
 		return true, jogador2.player
 	}
 
-	ProximaJogada(jogador1.player, jogador1.caractere)
-	if VerificaVencedor(matriz) {
+	if ExecutaJogada(jogador1.player, jogador1.caractere) {
 		return true, jogador1.player
 	}
 
-	ProximaJogada(jogador2.player, jogador2.caractere)
-	if VerificaVencedor(matriz) {
+	if ExecutaJogada(jogador2.player, jogador2.caractere) {
 		return true, jogador2.player
 	}
 
-	ProximaJogada(jogador1.player, jogador1.caractere)
-	if VerificaVencedor(matriz) {
+	if ExecutaJogada(jogador1.player, jogador1.caractere) {
 		return true, jogador1.player
 	}
 	return false, 0
