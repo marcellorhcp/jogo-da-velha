476c256eeaf6ac30bcae10a7d164da276fb74b2a
Marcelo Azevedo <marcellorhcp@gmail.com>
2022-06-13 17:53:50 -0300

    alterando nome da função próximaJogada para executaJogada

--- executaJogada.go
+++ executaJogada.go
@@ -2,16 +2,16 @@ package main
 
 import "fmt"
 
-func ProximaJogada(numeroDoJogador int, opcaoDeJogada string) {
+func ExecutaJogada(numeroDoJogador int, opcaoDeJogada string) bool {
 	fmt.Printf("\nJogador %v - Faça uma jogada - %s", numeroDoJogador, opcaoDeJogada)
 	fmt.Println("\n\n", RetornaResultadoParcialdaPartida())
 	fmt.Scan(&jogada)
 	if VerificaJogadaValida(jogada) {
 		MarcaPosicaoEscolhida(jogada, opcaoDeJogada)
-
 	} else {
 		fmt.Println("\nOpa, parece que você escolheu uma posição já ocupada")
 		fmt.Println("\nou digitou um número / caracter inválido")
-		ProximaJogada(numeroDoJogador, opcaoDeJogada)
+		ExecutaJogada(numeroDoJogador, opcaoDeJogada)
 	}
+	return VerificaVencedor(matriz)
 }
