e6db2d978dd4b3129c8ba799fadb98a01c0ac44d
Marcelo Azevedo <marcellorhcp@gmail.com>
2022-06-28 15:48:22 -0300

    alterando layout do tabuleiro

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
-import "fmt"
-
 type Jogador struct {
-	numero            int
-	caractere         string
-	quantidadeJogadas int
+	numero    int
+	caractere string
 }
 
 func main() {
-	var jogador1 = novoJogador(1, "", 0)
-	var jogador2 = novoJogador(2, "", 0)
+	var jogador1 = novoJogador(1, "")
+	var jogador2 = novoJogador(2, "")
 
 	//matriz representa as posições no jogo da velha
 	var matriz = [][]string{
@@ -19,20 +16,7 @@ func main() {
 		{" ", " ", " "},
 	}
 
-	fmt.Printf(`
-	# Bem-vindo ao Jogo da Velha
-	# O jogo funciona da seguinte forma:
-	# Cada número escolhido representa uma casa no jogo da velha
-	# A ordem das casas está de acordo com o teclado numérico
-	# Boa sorte
-	
-		| 7 | 8 | 9 |
-		------------
-		| 4 | 5 | 6 |
-		------------
-		| 1 | 2 | 3 |
-
-	`)
+	InstrucoesDoJogo()
 
 	EscolheCaractere(&jogador1, &jogador2)
 	InformaResultadoFinalDaPartida(IniciaJogo(&jogador1, &jogador2, matriz))
