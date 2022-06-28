c822c10dcef7ebd2840aa928e4647ba44e4bb9f7
Marcelo Azevedo <marcellorhcp@gmail.com>
2022-06-28 15:45:52 -0300

    adicionando função instruçõesDoJogo

---
+++ instrucoesDoJogo.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func InstrucoesDoJogo() {
+	fmt.Printf(`
+	# Bem-vindo ao Jogo da Velha
+	# O jogo funciona da seguinte forma:
+	# Cada número escolhido representa uma casa no jogo da velha
+	# A ordem das casas está de acordo com o teclado numérico
+	# Boa sorte
+		+---+---+---+
+		| 7 | 8 | 9 |
+		+---+---+---+
+		| 4 | 5 | 6 |
+		+---+---+---+
+		| 1 | 2 | 3 |
+		+---+---+---+
+
+	`)
+}
