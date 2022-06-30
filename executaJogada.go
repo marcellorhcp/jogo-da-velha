2d7eb4cd9e28c075d897bd15958e50496220f394
Marcelo Azevedo <marcellorhcp@gmail.com>
2022-06-29 21:04:03 -0300

    refatorando código para adicionar os pacotes model e view

--- executaJogada.go
+++ executaJogada.go
@@ -1,16 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/marcellorhcp/jogo-da-velha/model"
+	"github.com/marcellorhcp/jogo-da-velha/view"
+)
 
 //ExecutaJogada executa a opção escolhida pelo jogador caso ela esteja disponível
 //caso contrário, de forma recursiva, a função é chamada novamente até que a jogada seja válida
-func ExecutaJogada(jogador *Jogador, matriz [][]string) bool {
+func ExecutaJogada(jogador *model.Jogador, matriz [][]string) bool {
 	var jogada string
-	fmt.Printf("\nJogador %v - Faça uma jogada - %s", jogador.numero, jogador.caractere)
-	fmt.Println("\n\n", RetornaResultadoParcialdaPartida(matriz))
+	fmt.Printf("\nJogador %v - Faça uma jogada - %s", jogador.Numero, jogador.Caractere)
+	fmt.Println("\n\n", view.RetornaResultadoParcialdaPartida(matriz))
 	fmt.Scan(&jogada)
 	if VerificaJogadaValida(jogada, matriz) {
-		MarcaPosicaoEscolhida(jogada, jogador.caractere, matriz)
+		MarcaPosicaoEscolhida(jogada, jogador.Caractere, matriz)
 	} else {
 		fmt.Println("\nOpa, parece que você escolheu uma posição já ocupada")
 		fmt.Println("\nou digitou um número / caracter inválido")
